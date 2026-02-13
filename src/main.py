dd9efc697e098c5a427c0002e32c464290fd6d72
A7medElsharkawy <sharkawynoo271@gmail.com>
2026-02-13 01:00:43 -0700

    create on_event for fastapi

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from helper.config import Setting
-from routes import base
-from routes import data
+from routes import base, data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helper.config import get_settings
 
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client():
+    setting= get_settings()
+    app.mongo_conn = AsyncIOMotorClient(setting.MONGO_URI)
+    app.db_client = app.mongo_conn[setting.MONGO_DATABASE]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_conn.close()
+
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+
+
