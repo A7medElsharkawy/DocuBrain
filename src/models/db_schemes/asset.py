8489b34cca3b9f18677715de163b772e37e60402
A7medElsharkawy <sharkawynoo271@gmail.com>
2026-03-07 00:51:47 -0700

    formating

--- src/models/db_schemes/asset.py
+++ src/models/db_schemes/asset.py
@@ -1,20 +1,19 @@
-
-import datetime
+from datetime import datetime
 from pydantic import BaseModel, Field
 from typing import Optional
-from bson import ObjectId
+from bson.objectid import ObjectId
 
 class Asset(BaseModel):
     id : Optional[ObjectId] = Field(None, alias="_id")
     asset_project_id: ObjectId
     asset_type: str=Field(...,min_length=1)
     asset_name: str = Field(..., min_length=1)
-    asset_size: int = Field(ge=0, default=None)
+    asset_size: int = Field(ge=0, default=None) #ge is greater than or equal to and default is 0
     asset_cofig: dict = Field(default=None)
     asset_pushed_at: datetime = Field(default=datetime.utcnow)
 
 
-    class Config:
+    class Config: # this class is used to allow the arbitrary types allowed (ObjectId)
         arbitrary_types_allowed = True
     
     @classmethod
