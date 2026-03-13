9d042ff6921a1fcf2583e6b56cac4d8cdab36ebe
A7medElsharkawy <sharkawynoo271@gmail.com>
2026-03-13 05:02:59 -0600

    crerate llminterface abstract funcation for all provider

---
+++ src/stores/llm/LLMinterface.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class LLMInterface(ABC):
+
+    @abstractmethod
+    def set_generation_models(self, mode_id:str):
+        pass
+    @abstractmethod
+    def set_embedding_model(self, model_id: str):
+        pass
+    @abstractmethod
+    def generate_text(self, prompt: str,max_output_tokens: int ,chat_history:list=[],  temperature: float =None):
+        pass
+    @abstractmethod
+    def embed_text(self, text: str, document_type: str=None):
+        pass
+    @abstractmethod
+    def construct_prompt(self, prompt: str, role: str):
+        pass
