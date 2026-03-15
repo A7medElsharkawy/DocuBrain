f60cbd82be7bb5c7b7747ef1817461da9f55f8b8
A7medElsharkawy <sharkawynoo271@gmail.com>
2026-03-15 03:10:41 -0600

    create get_path for database

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -7,6 +7,16 @@ class BaseController:
         self.settings = get_settings()
         self.base_dir = os.path.dirname(os.path.dirname(__file__))
         self.file_dir = os.path.join(self.base_dir, "assets", "files")
+        self.db_dir = os.path.join(self.base_dir, "assets", "vectoreDB")
 
     def generate_random_string(self, length: int=12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    
+    def get_db_path(self,db_name):
+        db_path =  os.path.join(self.db_dir,db_name)
+
+        if not os.path.exists(db_path):
+            os.makedirs(db_path)
+        else:
+            return db_path
+            
